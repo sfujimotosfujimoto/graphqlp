ebc0743b0017e1d22cb6f6b8ca70a2c5e48bdd10
S Fujimoto <unicoshun@gmail.com>
2018-12-11 22:14:13 +0900

    48 add prisma

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -218,12 +218,16 @@ const Mutation = {
         db.comments.push(comment);
 
         pubsub.publish(`comment ${args.data.post}`, {
-            comment
+            comment: {
+                mutation: 'CREATED',
+                data: comment
+            }
         });
         return comment;
     },
     deleteComment(parent, args, {
-        db
+        db,
+        pubsub
     }, info) {
         const commentIndex = db.comments.findIndex((comment) => comment.id === args.id);
 
@@ -231,9 +235,47 @@ const Mutation = {
             throw new Error('Comment not found');
         }
 
-        const deletedComment = db.comments.splice(commentIndex, 1);
+        const [deletedComment] = db.comments.splice(commentIndex, 1);
+        pubsub.publish(`comment ${deletedComment.post}`, {
+            comment: {
+                mutation: 'DELETED',
+                data: deletedComment
+            }
+        });
+        return deletedComment;
+    },
+    updateComment(parent, args, {
+        db,
+        pubsub
+    }, info) {
 
-        return deletedComment[0];
+        const {
+            id,
+            data
+        } = args;
+        const comment = db.comments.find((comment) => comment.id === id);
+
+
+        if (!comment) {
+            throw new Error('Comment not found');
+        }
+
+        if (typeof data.text === 'string') {
+            comment.text = data.text;
+        }
+
+
+        pubsub.publish(`comment ${comment.post}`, {
+            comment: {
+                mutation: 'UPDATED',
+                data: comment
+            }
+        })
+
+        return comment
+
+
+
     }
 };
 
@@ -241,3 +283,9 @@ export {
     Mutation as
     default
 };
+
+
+// id: ID!
+//   text: String!
+//   author: User!
+//   post: Post!
